b8578d5e95fa7afffed6bf8e3b476cf19a1182e6
Jinpeng-He <18240890624@139.com>
2016-11-16 22:11:40 +0800

    create the hashtable lib
    a lib of hashtable useing c plus plus

---
+++ hashtable/include/Boolean.h
@@ -0,0 +1,16 @@
+#ifndef _BOOLEAN_H
+#define _BOOLEAN_H
+
+
+typedef unsigned char Boolean;
+
+#ifndef False
+const Boolean False = 0;
+#endif
+#ifndef True
+const Boolean True = 1;
+#endif
+
+
+
+#endif
